d847bd6ceecb7031dd439a14b3f270b3068e8e7d
Marcus <mooi0002@student.monash.edu>
2019-08-22 14:50:45 +1000

    Use ejs engine

--- Week5/server.js
+++ Week5/server.js
@@ -1,6 +1,24 @@
 let express = require('express');
-
+let bodyParser = require('body-parser');
 let app = express();
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+let urlencodedParser = bodyParser.urlencoded({ extended: false});
+let jsonParser = bodyParser.json();
+
 app.use(express.static('css'));
 app.use(express.static('views'));
+
+app.get('/', urlencodedParser, (_, res) => {
+    res.render('index.html');
+});
+
+app.get('/newTask', urlencodedParser, (_, res) => {
+    res.render('newTask.html');
+});
+
+app.get('/listTasks', urlencodedParser, (_, res) => {
+    res.render('listTasks.html');
+});
+
 app.listen(8080);

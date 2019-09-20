dd2f7985e3f9ec9cb8a5babb7493cf58cd347bee
Marcus <mooi0002@student.monash.edu>
2019-09-20 22:54:10 +1000

    Week 9 work

--- Week9/src/app/app.component.ts
+++ Week9/src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Book {
+  bookTitle: string,
+  pubDate: number,
+  bookType: string,
+  bookSum: string,
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  books: Array<Book> = [];
+  numOfHardCovers: number = 0;
   title = 'Week9';
+  bookTitle: string = '';
+  pubDate: number = Date.now();
+  bookType: string = 'Hard Cover';
+  bookSum: string = '';
+
+  ngOnInit() {} 
+
+  saveBook() {
+    this.books.push({
+      bookTitle: this.bookTitle,
+      pubDate: this.pubDate,
+      bookType: this.bookType,
+      bookSum: this.bookSum,
+    });
+    if (this.bookType === 'Hard Cover') {
+      this.numOfHardCovers += 1;
+    };
+  }
+
+  deleteBook(bookNo: number): void {
+    this.books.splice(bookNo, 1);
+  }
+
+  deleteHardCovers():void {
+    this.books = this.books.filter(x => x.bookType !== 'Hard Cover')
+    this.numOfHardCovers = 0;
+  }
 }

77b5ba8d0b4c2b76ab7d32b89426f6d9b0d5d5e8
Marcus <mooi0002@student.monash.edu>
2019-10-21 16:43:43 +1100

    Half of Week12 tasks

--- Week12/src/app/app.component.ts
+++ Week12/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import * as io from "socket.io-client";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'Week12';
+  userName: string = "";
+  userNameInput: string;
+  messageText: string;
+  messages: Array<any> = [];
+  socket: SocketIOClient.Socket;
+  constructor() {
+    this.socket = io.connect();
+  }
+  ngOnInit() {
+    this.messages = new Array();
+    this.listen2Events();
+  }
+  listen2Events() {
+    this.socket.on("msg", data => {
+      this.messages.push(data);
+    });
+    console.log(this.messages);
+  }
+  sendMessage() {
+    this.socket.emit("newMsg", {
+      from: this.userName,
+      msg: this.messageText,
+    });
+    this.messageText = "";
+  }
+  setUserName() {
+    this.userName = this.userNameInput;
+    this.userNameInput = "";
+  }
 }
